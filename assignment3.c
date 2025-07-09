4febb4fdba15e07d4d903a29e3ddff0b78e4d2fd
Chisquare007 <chisquarevic@gmail.com>
2025-07-09 22:33:56 +0100

    correction on assignment3

--- assignment3.c
+++ assignment3.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 int main() {
- int i;
+	int i,j;
 
-    for (i = 1; i <= 10; i++) { 
-        printf("2 * %d = %d \n",i,2*i);
-    }
-	putchar('\n'); 
-    for (i = 1; i <= 10; i++) { 
-        printf("3 * %d = %d \n",i,3*i);
-    }
-    	printf("\n");
-    for (i = 1; i <= 10; i++) { 
-        printf("4 * %d = %d \n",i,4*i);
-    }
-    	putchar('\n');
-  return 0;
+	for (i = 2; i <= 4; i++) 
+	{ 
+		for(j=1; j<=10; j++)
+		{   
+			printf("%d x %d = %d\n", i,j,i*j);
+		}
+
+		printf("\n");
+	}
+	return 0;
 }
