ea2340a9eaab616c20b808f76ea9f059c8bcd2e1
Chisquare007 <chisquarevic@gmail.com>
2025-07-09 20:56:12 +0100

    assignment on times table

---
+++ assignment3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+ int i;
+
+    for (i = 1; i <= 10; i++) { 
+        printf("2 * %d = %d \n",i,2*i);
+    }
+	putchar('\n'); 
+    for (i = 1; i <= 10; i++) { 
+        printf("3 * %d = %d \n",i,3*i);
+    }
+    	printf("\n");
+    for (i = 1; i <= 10; i++) { 
+        printf("4 * %d = %d \n",i,4*i);
+    }
+    	putchar('\n');
+  return 0;
+}
